0252ec4495c357c2d3c5a6e67b2a04150da34308
adyasha1018 <adyasha1018@users.noreply.github.com>
2023-10-18 14:30:28 +0200

    Update country-chart.component.ts

--- src/app/country-chart/country-chart.component.ts
+++ src/app/country-chart/country-chart.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PopulationDataService } from '../population-data.service';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-country-chart',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./country-chart.component.css']
 })
 export class CountryChartComponent {
+  pieChartData: any[] = [];
 
+  constructor(private populationService: PopulationDataService) {}
+
+  ngOnInit(): void {
+    this.populationService.getPopulationData().subscribe((data: any[]) => {
+      this.pieChartData = data;
+      this.createPieChart();
+    });
+  }
+
+  createPieChart() {
+    const ctx = document.getElementById('populationPieChart') as HTMLCanvasElement;
+    const myChart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        labels: this.pieChartData.map(country=>country.name),
+        datasets: [{
+          data: this.pieChartData.map(country=>country.population),
+          backgroundColor: ['red','blue','yellow','orange','green'],
+        }]
+      }
+    });
+  }
+  
 }
